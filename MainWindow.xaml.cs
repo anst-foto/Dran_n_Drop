12e48cbb1aae4aef1f42deecfab566003fd173ec
Starinin Andrey <starinin-andrey@ya.ru>
2025-09-17 20:33:51 +0300

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Dran_n_Drop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void _MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        var block = (TextBlock)sender;
+        DragDrop.DoDragDrop(block, block.Background, DragDropEffects.Copy);
+    }
+
+    private void _Drop(object sender, DragEventArgs e)
+    {
+        var shape = sender as Shape;
+        var brush = e.Data.GetData(typeof(SolidColorBrush));
+        shape.Fill = brush as Brush;
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace Dran_n_Drop;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ObservableCollection<SolidColorBrush> ListOfColors { get; set; } = 
+    [
+        new SolidColorBrush(Colors.Red),
+        new SolidColorBrush(Colors.Green),
+        new SolidColorBrush(Colors.Blue)
+    ];
+}
